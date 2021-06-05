cd5b49a32156def69807ed3b018faf2eed4a6bf3
Efriandika Pratama <efriandika@gmail.com>
2021-06-05 10:54:30 +0700

    Added unit test for pipes

--- src/app/shared/pipes/speed-unit.pipe.spec.ts
+++ src/app/shared/pipes/speed-unit.pipe.spec.ts
@@ -1,8 +1,25 @@
 import { SpeedUnitPipe } from './speed-unit.pipe';
 
-describe('SpeedUnitPipe', () => {
+describe('SpeedUnitPipe Testing', () => {
+  let pipe: SpeedUnitPipe;
+
+  beforeEach(() => {
+    pipe = new SpeedUnitPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new SpeedUnitPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform metric unit', () => {
+    expect(pipe.transform(100, 'metric')).toEqual('100 meter/s');
+  });
+
+  it('should transform imperial unit', () => {
+    expect(pipe.transform(100, 'imperial')).toEqual('100 miles/h');
+  });
+
+  it('should return original value when unit is not supported', () => {
+    expect(pipe.transform(100, 'someUnSupportedUnit')).toEqual('100');
   });
 });
