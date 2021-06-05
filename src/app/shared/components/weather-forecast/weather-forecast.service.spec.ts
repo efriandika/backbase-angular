f4824acbbf91d88dfeda231d2c4463deb22fb689
Efriandika Pratama <efriandika@gmail.com>
2021-06-05 10:36:08 +0700

    Added unit test for services

--- src/app/shared/components/weather-forecast/weather-forecast.service.spec.ts
+++ src/app/shared/components/weather-forecast/weather-forecast.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
 import { WeatherForecastService } from './weather-forecast.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../../../../environments/environment';
 
 describe('WeatherForecastService', () => {
   let service: WeatherForecastService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        WeatherForecastService
+      ]
+    });
+
     service = TestBed.inject(WeatherForecastService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should find one forecasted data by coordinate and units', () => {
+    const lon = 100;
+    const lat = 101;
+    const units = 'metric';
+
+    service.findOne(lon, lat, units).subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne(`${environment.backendBaseURL}/onecall?lon=${lon}&lat=${lat}&units=${units}&exclude=minutely,daily,alerts`);
+    expect(httpRequest.request.method).toEqual('GET');
   });
 });
--- src/app/shared/components/weather/weather.service.spec.ts
+++ src/app/shared/components/weather/weather.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
-
 import { WeatherService } from './weather.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../../../../environments/environment';
 
-describe('WeatherService', () => {
+describe('WeatherService Testing', () => {
   let service: WeatherService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        WeatherService
+      ]
+    });
+
     service = TestBed.inject(WeatherService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should find one data by city name and unit', () => {
+    const cityName = 'Jakarta';
+    const units = 'metric';
+
+    service.findOne(cityName, units).subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne(`${environment.backendBaseURL}/weather?q=${cityName}&units=${units}`);
+    expect(httpRequest.request.method).toEqual('GET');
   });
 });
